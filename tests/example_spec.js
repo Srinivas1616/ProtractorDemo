7a333d0b16b345bf0bad20fddb129e8053731fb7
Srinivas Natikar <Srinivas.Natikar@msg-global.com>
2020-07-14 13:03:25 +0530

    Second change commit in example_code

--- tests/example_spec.js
+++ tests/example_spec.js
@@ -5,8 +5,10 @@ describe('angularjs homepage', function() {
     element(by.model('yourName')).sendKeys('Julie');
 
     var greeting = element(by.binding('yourName'));
-
+    
     expect(greeting.getText()).toEqual('Hello Julie!');
+
+    browser.sleep(2000);
   });
 
   it('should greet the named user', function() {
